91b9ccdcbbb8f8bda6d31a82fd7bd6f5070b0bb1
Michael DeWitt <md@hackfoolery.com>
2014-04-29 12:38:22 -0400

    fix user wrappers

--- src/api/user.rb
+++ src/api/user.rb
@@ -1,6 +1,24 @@
 class User < API::Record
   attr_accessor :username, :name, :email, :remember_token
 
+  class << self
+    def create params={}
+      new parse(RestClient.post(url(base_route), params))
+    end
+    translate_exceptions :create
+
+    def find
+      record = new parse(RestClient.get(authenticated_url(base_route)))
+      record.send(:make_old)
+      record
+    end
+    translate_exceptions :find
+
+    def fields
+      %w{username email name remember_token}
+    end
+  end
+
   def initialize params={}
     @username = params[:username]
     @name = params[:name]
@@ -9,30 +27,11 @@ class User < API::Record
     super params
   end
 
-  def self.fields
-    %w{username email name remember_token}
+  def save
+    url = authenticated_url(self.class.base_route, id)
+    RestClient.patch url, to_params 
   end
-
-  def create params={}
-    new(params).save(params)
-  end
-
-   def find string
-     record = new parse(RestClient.get(url(base_route, id)))
-     record.send(:make_old)
-     record
-   end
-
-  def save params={}
-    if new_record? #create
-      RestClient.post url(self.class.base_route, id), params
-      make_old
-    else #update
-      url = authenticated_url(self.class.base_route, id)
-      RestClient.patch url, to_params 
-    end
-    self
-  end
+  translate_exceptions :save
 
   def to_params
     { username: username,
